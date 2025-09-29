bc1160a6e2769e151088a0a7a6884d2c4544dfb6
Gabriel <gabrielsrizzo@hotmail.com>
2025-09-29 20:19:41 -0300

    Criado sistema básico de finanças

---
+++ financas.py
@@ -0,0 +1,15 @@
+print ("===SISTEMA DE FINANÇAS===")
+
+saldo = 0
+print(f"Saldo inicial: R${saldo}")
+
+receita = 1000
+saldo = saldo + receita
+print(f"Recebi R${receita} de salário")
+print(f"Saldo atual: R${saldo}")
+
+
+gasto = 200
+saldo = saldo - gasto
+print(f"Gastei R${gasto} no supermercado")
+print(f"Saldo atual: R${saldo}")
